dae6d7bc712e9f07849cd876ed98e8dfc34ce0b9
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-04-13 16:44:48 -0500

    few bugs

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
-// Importing necessary packages and files
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from './state';
-
-// Importing packages for persisting Redux state
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+//importing react and react-dom libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
@@ -17,33 +15,28 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-// Configuration for Redux state persistence
-const persistConfig = { key: 'root', storage, version: 1 };
+//redux-persist configuration
+const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
-
-// Creating the Redux store
 const store = configureStore({
-  reducer: persistedReducer, // Setting the reducer to use the persisted reducer
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      // Ignoring certain actions that are not serializable
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
-
-// Rendering the App component with the Redux store and persistor
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//rendering the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* Wrapping the App component in a PersistGate component to ensure that the store is rehydrated before rendering */}
-      <PersistGate loading={null} presistor={persistStore(store)}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>
